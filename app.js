b76556755a790b57b9438dc20f178ae859ace694
ckeuken <50991520+ckeuken@users.noreply.github.com>
2021-04-29 07:14:26 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+var tl= gsap.timeline({defaults: {duration: 1.3, ease: 'elastic'}});
+
+tl.from('.left', {x: '-120%'})
+  .from('.right', {x: '-100vw'})
+  .fromTo('#nav_bar', {opacity: 0}, {opacity: 1})
+  
+  
+  
+
+
+
+
+const scroll = new SmoothScroll('a[href*="#"]');
+
+  
+  
+
+ 
+
+
+
+
+
+
